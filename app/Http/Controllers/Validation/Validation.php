6453de0c70ef7cd9bce567ec151de0579c96b1f3
Cristhian Mariño <crimarino@uniboyaca.edu.co>
2019-09-10 10:32:27 -0500

    Revisando el procedimiento almacenado, no se porque no sirve

--- app/Http/Controllers/Validation/Validation.php
+++ app/Http/Controllers/Validation/Validation.php
@@ -10,10 +10,16 @@ class Validation extends Controller
 {
     public function storeProcedure(Request $request)
     {
-        $user = GameModel::select('call traerUsuario(?,?)', [
-            'emailPas' =>$request->email,
-            'passwordPas' =>$request->password
-            ]);
-        return response()->json($user);
+        $email = $request->email;
+        $password = $request->password;
+
+        $user = GameModel::select('call traerUsuario(?,?)', [$email, $password]);
+
+        /* $user = GameModel::select('call traerUsuario(?,?)', 
+            [
+                'emailPas' =>$request->email,
+                'passwordPas' =>$request->password
+            ]); */
+        return response()->json($user); 
     }
 }
