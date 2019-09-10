c4dbb2b146de3d71ab97f8631439c99a12327748
Cristhian Mariño <crimarino@uniboyaca.edu.co>
2019-09-10 10:59:26 -0500

    Agregado archivos de componentes, servicios y modelos

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    GameFormComponent,
+    GameListComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
