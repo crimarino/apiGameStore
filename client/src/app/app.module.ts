ae35c9eea35fd52f01b1615206f2f64880878949
Cristhian Mariño <crimarino@uniboyaca.edu.co>
2019-09-11 11:41:01 -0500

    Configurando las carpetas de primeng

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,34 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
-import { PrincipalComponent } from './components/principal/principal.component';
-import { GameFormComponent } from './components/game-form/game-form.component';
-import { GameListComponent } from './components/game-list/game-list.component';
-import { SigninComponent } from './components/signin/signin.component';
-import { SignupComponent } from './components/signup/signup.component';
+import {GamesService } from './services/games.service';
+import { UsersService } from './services/users.service';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MenuModule} from 'primeng/menu';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PrincipalComponent,
-    GameFormComponent,
-    GameListComponent,
-    SigninComponent,
-    SignupComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MenuModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    GamesService,
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
