9971940b8f321eb96fd2c4a75f2ded78e8414d3a
Cristhian Mariño <crimarino@uniboyaca.edu.co>
2019-09-10 19:30:37 -0500

    Pasamos a configurar el primeng

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PrincipalComponent } from './components/principal/principal.component';
-
 
 const routes: Routes = [
 
-  {path: '', component:PrincipalComponent}
-
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
